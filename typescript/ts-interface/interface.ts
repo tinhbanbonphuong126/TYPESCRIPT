c8d7bab4f2b2353ae4f2c8fbf062fcebe9aea0fb
Que Pham <adnetplus.dev@gmail.com>
2018-06-06 01:20:02 +0700

    #QuePham: The basic about Interface.

---
+++ typescript/ts-interface/interface.ts
@@ -0,0 +1,22 @@
+interface FirstName {
+    firstName: string
+}
+
+const face = (people: FirstName): void => {
+    console.log(`Hello ${people['firstName']}`);
+};
+
+
+const changeFace = (people: FirstName): void => {
+    people.firstName = 'Hello world';
+};
+
+let people = {
+    firstName: 'Pham',
+    lastName: 'Que'
+};
+
+face(people);
+changeFace(people);
+face(people);
+
