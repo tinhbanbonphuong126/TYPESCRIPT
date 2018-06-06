4112387813972d60f572837bb71ac58698075b8e
Que Pham <adnetplus.dev@gmail.com>
2018-06-06 11:48:45 +0700

    #QuePham: Generic Constraint.

--- typescript/ts-generic/generic.ts
+++ typescript/ts-generic/generic.ts
@@ -34,6 +34,46 @@
         args.forEach((element) => console.log(element));
     };
 
-    printAll(["Apple", "Banana"]);
+    // printAll(["Apple", "Banana"]);
+
+    // Using Generic Type.
+
+    const echo2: <T>(data: T) => T = betterEcho;
+
+    console.log(echo2<string>("Some things"));
+
+    // Create Generic Class.
+
+    class SimpleMathGeneric<T> {
+        baseValue: T;
+        multiplyValue: T;
+        cal(): number {
+            return +this.baseValue * +this.multiplyValue;
+        }
+
+
+        constructor(baseValue: T, multiplyValue: T) {
+            this.baseValue = baseValue;
+            this.multiplyValue = multiplyValue;
+        }
+    }
+
+    const simpleMath = new SimpleMathGeneric<number>(12, 23);
+    console.log(simpleMath.cal());
+
+
+    // Constraint.
+    class ConstraintClass<T extends number | string> {
+        baseValue: T;
+        cal(): T {
+            return this.baseValue;
+        }
+    }
+
+    const constraintClass = new ConstraintClass<string>()
+    constraintClass.baseValue = 'Text Here';
+    console.log(constraintClass.cal());
+
+
 
 })();
